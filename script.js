255187bae7f63819dd6afe7a97333a19c346dd32
BigRadd <89674000+BigRadd@users.noreply.github.com>
2025-02-15 12:15:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.download-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('¡Gracias por descargar! ¡Disfruta del archivo!');
+    });
+});
